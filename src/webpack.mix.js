3a108fba4851d4e95721a387cf4ca8da9d17b8e1
大塚みちあ <ootsukamichia@ootsukamichianoMac-mini.local>
2023-02-14 19:55:35 +0900

    add:docker-laravel-vue

---
+++ src/webpack.mix.js
@@ -0,0 +1,16 @@
+const mix = require('laravel-mix');
+require('laravel-mix-vue3');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.vue3('resources/js/app.ts', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');

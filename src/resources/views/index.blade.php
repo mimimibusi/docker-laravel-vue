36977a5785a06325e21d0fca3c2e1961bf277afa
大塚みちあ <ootsukamichia@ootsukamichianoMac-mini.local>
2023-02-14 20:07:36 +0900

    chore:index.bladeの<script>のsrcはjs/app.jsのままでよかった

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="js">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +8,6 @@
 </head>
 <body>
     <div id="app"></div>
-    <script src="js/app.ts"></script>
+    <script src="js/app.js"></script>
 </body>
 </html>

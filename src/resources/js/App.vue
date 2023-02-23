3fb6c63bc12a8acc7e6b565aba24d48d2e3a7477
大塚みちあ <ootsukamichia@ootsukamichianoMac-mini.local>
2023-02-23 15:16:18 +0900

    fix:typescriptで動くようにした

--- src/resources/js/App.vue
+++ src/resources/js/App.vue
@@ -1,18 +1,18 @@
 <template>
-    <div>{{ hello }}</div>
-  </template>
-  
-  <script lang="ts">
-  import { ref } from "vue";
-  export default {
-    setup() {
-        const hello = ref(
-            "Hello From TypeScript"
-        )
-  
-        return {
-            hello
-        }
-    },
-  };
-  </script>
+  <div>{{ hello }}</div>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+export default {
+  setup() {
+    const hello = ref<string>(
+      "TypeScript"
+    );
+
+    return {
+      hello
+    };
+  },
+};
+</script>

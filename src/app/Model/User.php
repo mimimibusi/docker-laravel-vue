31f099796e0b0ba09b997b89912230c51adfa31b
大塚みちあ <ootsukamichia@ootsukamichianoMac-mini.local>
2023-04-26 08:13:21 +0900

    fix:ログインページを表示してログイン、新規アカウント作成できるようにした(ログアウトができない)

--- src/app/Model/User.php
+++ src/app/Model/User.php
@@ -3,9 +3,51 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
-class User extends Model
+
+class User extends Model implements AuthenticatableContract
+
 {
+    use Authenticatable;
+
+    protected $fillable = [
+        'name', 'email', 'password'
+    ];
+
+    //ここから下いらないかも
+    // 以下のメソッドはAuthenticatableインターフェースの実装に必要
+    public function getAuthIdentifierName()
+    {
+        return 'id';
+    }
+
+    public function getAuthIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
+
     public function getData(int $id)
     {
         $user = self::find($id);

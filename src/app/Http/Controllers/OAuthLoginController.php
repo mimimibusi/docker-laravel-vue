31b468e196a1e025fb20964de00b3d888dc671ba
大塚みちあ <ootsukamichia@ootsukamichianoMac-mini.local>
2023-05-12 19:55:43 +0900

    chore:リフレッシュトークンを取得できそうなのをコメントアウトして書いた

--- src/app/Http/Controllers/OAuthLoginController.php
+++ src/app/Http/Controllers/OAuthLoginController.php
@@ -3,33 +3,60 @@
 namespace App\Http\Controllers;
 
 use App\Model\User;
+use Google_Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 use Socialite;
 
 class OAuthLoginController extends Controller
 {
     public function __construct(
-        User $user
+        User $user,
+        Google_Client $client
     ){
         $this->user = $user;
+        $this->client = $client;
     }
 
     public function getGoogleAuth()
     {
+        // $client = new Google_Client();
+        // $this->client->setAccessType('offline');  //オフライン時にアクセストークンを更新
+        // $this->client->setApprovalPrompt("force"); //承認プロンプトの動作（force: 承認UIを強制的に表示する）       
+        // $this->client->setScopes(\Google_Service_Calendar::CALENDAR_READONLY); //スコープの設定        
+        // $this->client->setAuthConfig(storage_path('client_secret_35578369161-c1tfvcftqi12s64unt95io9s3m5pp5bg.apps.googleusercontent.com (1).json'));
+
+        // $auth_url = $this->client->createAuthUrl();
+        // \Log::info($auth_url);
+        // return Redirect::to($auth_url)->send();
+        
         return Socialite::driver('google')->redirect();
     }
 
-    public function authGoogleCallBack()
+    public function authGoogleCallBack(Request $request)
     {
+        \Log::info($request);
+        // $data = $this->client->fetchAccessTokenWithAuthCode($request->input('code'));   
+        // \Log::info($data);
+        // $this->client->setAccessToken(json_encode($data));  //アクセストークンはJSONで設定
+        // $refresh_token = $data['refresh_token'];
+        // \Log::info($data);
+        // \Log::info($refresh_token);
+
+        // //DBに取得したトークンを保存
+        // $user_model = Auth::user();
+        // $user_model->fill([
+        //     'access_token' => $data,
+        //     'refresh_token' => $refresh_token,
+        // ])->save();
+
         $googleUser = Socialite::driver('google')->user();
-        \Log::info($googleUser->token);
-        \Log::info($googleUser->refreshToken);
+        \Log::info(print_r($googleUser, true));
         $authUser = Auth::user();
         $authUser->access_token = $googleUser->token;
-        $authUser->refresh_token = $googleUser->refreshToken;
+        // $authUser->refresh_token = $googleUser->refreshToken;
         $authUser->save();
-        // $this->user->where('id', Auth::id())->update(['access_token' => $googleUser->token]);
         return redirect('/setting');
     }
 

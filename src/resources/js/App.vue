7fe02e117553e2ba8e0c4b4d89762daa6e513941
俺です <hurumura1120@icloud.com>
2023-07-01 11:56:02 +0900

    fix compile err

--- src/resources/js/App.vue
+++ src/resources/js/App.vue
@@ -1,39 +1,39 @@
 <template>
-  <Header />
-  <div>{{ hello }}</div>
-  <div>{{ user }}</div>
-  <router-view></router-view>
+    <div>
+        <Header />
+        <div>{{ hello }}</div>
+        <div>{{ user }}</div>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script lang="ts">
-import { onMounted, ref } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import axios from "axios";
 import Header from "./components/Header.vue";
 import Friend from "./components/Friends/Index.vue";
 import Setting from "./components/Settings/Index.vue";
-export default {
-  components: {
-    Header,
-    Friend,
-    Setting,
-  },
-  setup() {
-    const hello = ref<string>(
-      "TypeScript"
-    );
-    const user = ref<string>('');
-    const getUsers = async () => {
-      const {data} = await axios.get('/index');
-      console.log(data);
-      user.value = data.name;
-    }
-    onMounted(()=>{
-      getUsers();
-    })
-    return {
-      hello,
-      user
-    };
-  },
-};
+export default defineComponent({
+    components: {
+        Header,
+        Friend,
+        Setting,
+    },
+    setup() {
+        const hello = ref("TypeScript");
+        const user = ref("");
+        const getUsers = async () => {
+            const { data } = await axios.get("/index");
+            console.log(data);
+            user.value = data.name;
+        };
+        onMounted(() => {
+            getUsers();
+        });
+        return {
+            hello,
+            user,
+        };
+    },
+});
 </script>
--- src/resources/js/components/Settings/Index.vue
+++ src/resources/js/components/Settings/Index.vue
@@ -1,45 +1,47 @@
 <template>
-	<div>{{ title }}</div>
-	<a href="login/google-oauth">Google認証aタグ</a>
-	<!-- <a href="login/google-oauth" v-if="accessTokenFlag">Google認証aタグ</a> -->
+    <div>
+        <div>{{ title }}</div>
+        <a href="login/google-oauth">Google認証aタグ</a>
+        <!-- <a href="login/google-oauth" v-if="accessTokenFlag">Google認証aタグ</a> -->
+    </div>
 </template>
 
 <script>
-import { ref, reactive, onMounted } from 'vue';
-import axios from 'axios';
+import { ref, onMounted } from "vue";
+import axios from "axios";
 
-export default{
-	name: 'Setting',
-	setup(){
-		const title = ref('せっていだよー＾＾');
-		const accessTokenFlag = ref(true); //vuexにユーザー情報持たせてそれで判定しても良いかも
+export default {
+    name: "Setting",
+    setup() {
+        const title = ref("せっていだよー＾＾");
+        const accessTokenFlag = ref(true); //vuexにユーザー情報持たせてそれで判定しても良いかも
 
-		// const judgeHaveAccessToken = async ()=>{
-		// 	await axios.get('/judgeHaveAccessToken').then((res)=>{
-		// 		accessTokenFlag.value = res.data.result;
-		// 		console.log(accessTokenFlag.value);
-		// 	}).catch((error)=>{
-		// 		console.log('うまくいかなかったです');
-		// 	})
-		// }
+        // const judgeHaveAccessToken = async ()=>{
+        // 	await axios.get('/judgeHaveAccessToken').then((res)=>{
+        // 		accessTokenFlag.value = res.data.result;
+        // 		console.log(accessTokenFlag.value);
+        // 	}).catch((error)=>{
+        // 		console.log('うまくいかなかったです');
+        // 	})
+        // }
 
-		const getCalendar = async ()=>{
-			if (accessTokenFlag) {
-				await axios.get('/googleCalendar').then((res)=>{
-					console.log(res);
-				});
-			}
-			console.log('せっていだしょー');
-		}
+        const getCalendar = async () => {
+            if (accessTokenFlag) {
+                await axios.get("/googleCalendar").then((res) => {
+                    console.log(res);
+                });
+            }
+            console.log("せっていだしょー");
+        };
 
-		onMounted(()=>{
-			// judgeHaveAccessToken();
-			getCalendar();
-		})
-		return {
-			title,
-			accessTokenFlag,
-		}
-	}
-}
+        onMounted(() => {
+            // judgeHaveAccessToken();
+            getCalendar();
+        });
+        return {
+            title,
+            accessTokenFlag,
+        };
+    },
+};
 </script>

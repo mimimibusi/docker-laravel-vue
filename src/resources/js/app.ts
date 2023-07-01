49f54493fabd3b36a4da56bb793fb41031ce0574
俺です <hurumura1120@icloud.com>
2023-07-01 11:55:25 +0900

    to ts file

---
+++ src/resources/js/app.ts
@@ -0,0 +1,16 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+import { createApp } from "vue";
+import App from "./App.vue";
+import { setupCalendar } from "v-calendar";
+import { router } from "./routes/router"; //app.tsだとここがanyとかでエラー
+import ExampleComponent from "./components/ExampleComponent.vue";
+
+const app = createApp(App);
+app.use(router);
+app.use(setupCalendar, {});
+app.mount("#app");
+app.component("example-component", ExampleComponent);
